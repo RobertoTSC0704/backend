944407182ba7729c0ac946ef0173bba773d93450
RobertoTSC0704 <l21450637@itz.edu.mx>
2024-12-14 19:48:40 -0600

    Agregar url frontend render y home del api

--- src/app.js
+++ src/app.js
@@ -17,13 +17,16 @@ const __dirname = path.dirname(__filename);
 import authRoutes from './routes/auth.routes.js';
 //importamos los rutas para usuarios
 import productRoutes from './routes/products.routes.js';
+import { version } from 'os';
 
 const app = express();
 app.use(cors({
     origin:['http://localhost:5173',
         'http://localhost:4000',
         'http://localhost',
-        process.env.BASE_URL
+        process.env.BASE_URL_BACKEND,
+        process.env.BASE_URL_FRONTEND
+
     ],
     credentials:true
 }));
@@ -39,4 +42,15 @@ app.use(express.urlencoded({extended:false}));
 app.use('/api/', authRoutes);
 app.use('/api/', productRoutes)
 
+app.get('/', (req, res) =>{
+    res.json({
+        mensaje: "Bienvenido al API REST de Productos",
+        version: "1.0.0",
+        rutasDisponibles: [
+            {endpoint: "/api/register", metodo: "POST", descripcion: "Crea un nuevo"},
+            {endpoint: "/api/login", metodo: "POST", descripcion: "Para iniciar sesión"}
+        ]
+
+    });
+});
 export default app;
